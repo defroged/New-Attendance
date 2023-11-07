9d80502824b95a65ceac5b7703aa297f325e8e04
Ron Ward <defroged@gmail.com>
2023-11-07 12:03:38 +0900

    added prev & future buttons

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 window.onload = function() {
     const days = document.querySelectorAll('.day');
-    const today = new Date();
-    const firstDayOfWeek = today.getDate() - today.getDay();
+    let currentWeekStart = new Date();
 
-    for (let i = 0; i < days.length; i++) {
-        const dayDate = new Date(today.getFullYear(), today.getMonth(), firstDayOfWeek + i);
-        days[i].innerText = dayDate.toDateString();
-        if (i === today.getDay()) {
-            days[i].classList.add('current-day');
+    function updateCalendar(weekStart) {
+        const firstDayOfWeek = weekStart.getDate() - weekStart.getDay();
+
+        for (let i = 0; i < days.length; i++) {
+            const dayDate = new Date(weekStart.getFullYear(), weekStart.getMonth(), firstDayOfWeek + i);
+            days[i].innerText = dayDate.toDateString();
+            days[i].classList.remove('current-day');
+            if (dayDate.toDateString() === new Date().toDateString()) {
+                days[i].classList.add('current-day');
+            }
         }
     }
+
+    document.getElementById('prevWeek').addEventListener('click', function() {
+        currentWeekStart.setDate(currentWeekStart.getDate() - 7);
+        updateCalendar(currentWeekStart);
+    });
+
+    document.getElementById('nextWeek').addEventListener('click', function() {
+        currentWeekStart.setDate(currentWeekStart.getDate() + 7);
+        updateCalendar(currentWeekStart);
+    });
+
+    updateCalendar(currentWeekStart);
 };
