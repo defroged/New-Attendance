df6a491639899af97f59d56c405b3ba3a8fa1caf
Ron Ward <defroged@gmail.com>
2023-11-07 13:54:53 +0900

    added Google fetch 2

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 window.onload = function() {
-    // Define the days of the calendar
     const days = document.querySelectorAll('.day');
     let currentWeekStart = new Date();
 
-    // Function to update the calendar display
     function updateCalendar(weekStart) {
         const firstDayOfWeek = new Date(weekStart);
         firstDayOfWeek.setDate(weekStart.getDate() - weekStart.getDay());
@@ -21,44 +19,13 @@ window.onload = function() {
         }
     }
 
-    // Function to handle the OAuth 2.0 redirect and extract the access token
-    function handleAuthRedirect() {
-        if (window.location.hash) {
-            const hash = window.location.hash.substring(1);
-            const params = new URLSearchParams(hash);
-            const accessToken = params.get('access_token');
+    function fetchEvents() {
+        const apiKey = 'AIzaSyBTp54-gKed7hQWoqSxWT2vaz5vrUMQOvc';
+        const calendarId = 'ronward.english@gmail.com';
+        const timeMin = new Date().toISOString(); // Start from the current date
+        const url = `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(calendarId)}/events?key=${apiKey}&timeMin=${timeMin}`;
 
-            if (accessToken) {
-                console.log('Access Token:', accessToken);
-                document.getElementById('authorize_button').style.display = 'none';
-                fetchEvents(accessToken); // Fetch events after getting the access token
-            }
-        } else {
-            document.getElementById('authorize_button').style.display = 'block';
-        }
-    }
-
-    // Attach the click event to the authorization button
-    document.getElementById('authorize_button').onclick = function() {
-        const client_id = '995403920982-a2hj9cvj8ngmk91q5pr22eo05gfsmc0d.apps.googleusercontent.com';
-        const redirect_uri = 'https://new-attendance.vercel.app/oauth2callback';
-        const scope = 'https://www.googleapis.com/auth/calendar.events.readonly';
-        const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=token&scope=${scope}`;
-
-        window.location = url;
-    };
-
-    // Function to fetch events from Google Calendar
-    function fetchEvents(accessToken) {
-        const calendarId = 'primary';
-        const url = `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(calendarId)}/events`;
-
-        fetch(url, {
-            headers: {
-                'Authorization': `Bearer ${accessToken}`,
-                'Accept': 'application/json'
-            }
-        })
+        fetch(url)
         .then(response => response.json())
         .then(data => {
             console.log('Events:', data.items);
@@ -69,20 +36,18 @@ window.onload = function() {
         });
     }
 
-    // Event listeners for navigating weeks
     document.getElementById('prevWeek').addEventListener('click', function() {
         currentWeekStart.setDate(currentWeekStart.getDate() - 7);
         updateCalendar(currentWeekStart);
+        fetchEvents(); // You may want to fetch events for this new week
     });
 
     document.getElementById('nextWeek').addEventListener('click', function() {
         currentWeekStart.setDate(currentWeekStart.getDate() + 7);
         updateCalendar(currentWeekStart);
+        fetchEvents(); // You may want to fetch events for this new week
     });
 
-    // Call the function to check for OAuth redirection
-    handleAuthRedirect();
-
-    // Initialize the calendar display for the current week
-    updateCalendar(currentWeekStart);
+    updateCalendar(currentWeekStart); // Initialize the calendar
+    fetchEvents(); // Fetch events for the current week
 };
