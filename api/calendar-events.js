c644fafac2a86b52b587e0dee91d691ec36e71a8
Ron Ward <defroged@gmail.com>
2023-11-07 14:24:33 +0900

    console logs in serverless

--- api/calendar-events.js
+++ api/calendar-events.js
@@ -1,15 +1,25 @@
-// This is an example using node-fetch, which you may need to install as a dependency
-// You can install it by running `npm install node-fetch` in your project directory
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-    const calendarId = 'ronward.english@gmail.com';
-    const apiKey = process.env.GOOGLE_CALENDAR_API_KEY; // The API key is stored in an environment variable
-    const timeMin = new Date().toISOString(); // Start from the current date
-    const url = `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(calendarId)}/events?key=${apiKey}&timeMin=${timeMin}`;
+    const calendarId = 'ronward.english@gmail.com';  // Ensure this is the public calendar ID
+    const apiKey = process.env.GOOGLE_CALENDAR_API_KEY;  // Make sure the API key is set in Vercel environment variables
+    const timeMin = new Date().toISOString();
+    const timeMax = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toISOString(); // Optional: Set timeMax to limit events to the next 7 days
+    const url = `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(calendarId)}/events?key=${apiKey}&timeMin=${timeMin}&timeMax=${timeMax}&singleEvents=true&orderBy=startTime`;
+
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Another option is to restrict the origin to your domain, for example:
+    // res.setHeader('Access-Control-Allow-Origin', 'https://new-attendance.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
     try {
         const calendarResponse = await fetch(url);
+        if (!calendarResponse.ok) {
+            throw new Error(`Failed to fetch calendar events: ${calendarResponse.statusText}`);
+        }
         const calendarData = await calendarResponse.json();
         res.status(200).json(calendarData);
     } catch (error) {
