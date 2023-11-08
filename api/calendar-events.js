951420feb23830231f5b442c83cb40f234abae0d
Ron Ward <defroged@gmail.com>
2023-11-08 14:22:05 +0900

    ROLLBACK3

--- api/calendar-events.js
+++ api/calendar-events.js
@@ -1,13 +1,13 @@
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
-    const calendarId = 'ronward.english@gmail.com';
-    const apiKey = process.env.GOOGLE_CALENDAR_API_KEY;
-    const timeMin = req.query.timeMin;
-    const timeMax = req.query.timeMax;
+    const calendarId = 'ronward.english@gmail.com';  // Ensure this is the public calendar ID
+    const apiKey = process.env.GOOGLE_CALENDAR_API_KEY;  // Make sure the API key is set in Vercel environment variables
+    const timeMin = new Date().toISOString();
+    const timeMax = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toISOString(); // Optional: Set timeMax to limit events to the next 7 days
     const url = `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(calendarId)}/events?key=${apiKey}&timeMin=${timeMin}&timeMax=${timeMax}&singleEvents=true&orderBy=startTime`;
-    
-	// Set CORS headers
+
+    // Set CORS headers
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*');
     // Another option is to restrict the origin to your domain, for example:
