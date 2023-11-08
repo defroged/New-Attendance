a32c0f11a7bd4e4143a911162bc159c78209f658
Ron Ward <defroged@gmail.com>
2023-11-08 14:17:16 +0900

    ROLLBACK

--- script.js
+++ script.js
@@ -2,73 +2,76 @@ window.onload = function() {
     const days = document.querySelectorAll('.day');
     let currentWeekStart = new Date();
 
-    // Updated to use local time calculations
     function updateCalendar(weekStart) {
         const firstDayOfWeek = new Date(weekStart);
-        firstDayOfWeek.setDate(firstDayOfWeek.getDate() - firstDayOfWeek.getDay());
+        firstDayOfWeek.setDate(weekStart.getDate() - weekStart.getDay());
         for (let i = 0; i < days.length; i++) {
             const dayDate = new Date(firstDayOfWeek);
-            dayDate.setDate(dayDate.getDate() + i);
+            dayDate.setDate(firstDayOfWeek.getDate() + i);
 
             const dateDiv = days[i].querySelector('.date');
             dateDiv.innerText = dayDate.toDateString();
-            days[i].setAttribute('data-date', dayDate.toISOString().split('T')[0]);
+
+            days[i].classList.remove('current-day');
+            if (dayDate.toDateString() === new Date().toDateString()) {
+                days[i].classList.add('current-day');
+            }
         }
     }
 
-    // Simplified fetchEvents function
     function fetchEvents() {
-        fetch(`/api/calendar-events`)
-        .then(response => response.json())
-        .then(data => {
-            console.log('Raw event data:', data);
-            displayEvents(data.items);
-        })
-        .catch(error => {
-            console.error('Error fetching events:', error);
-        });
-    }
+    fetch('/api/calendar-events')
+    .then(response => response.json())
+    .then(data => {
+        displayEvents(data.items);
+    })
+    .catch(error => {
+        console.error('Error fetching events:', error);
+    });
+}
 
-    function displayEvents(events) {
-        document.querySelectorAll('.time-slot').forEach(slot => slot.innerHTML = '');
+function displayEvents(events) {
+    // Clear previous events
+    document.querySelectorAll('.time-slot').forEach(slot => {
+        slot.innerHTML = ''; 
+    });
 
-        events.forEach(event => {
-            const eventElement = document.createElement('div');
-            eventElement.classList.add('event');
-            eventElement.innerText = event.summary;
+    events.forEach(event => {
+        // Create an event element
+        const eventElement = document.createElement('div');
+        eventElement.classList.add('event');
+        eventElement.innerText = event.summary;
 
-            // Assuming event.start.dateTime is in local time
-            const eventStart = new Date(event.start.dateTime || event.start.date);
-            const startHour = eventStart.getHours();
-            const eventDateISO = eventStart.toISOString().split('T')[0];
+        // Parse the start time of the event
+        const eventStart = new Date(event.start.dateTime || event.start.date);
+        const startHour = eventStart.getHours();
+        const eventDay = eventStart.getDay(); // This gets the day of the week (0 is Sunday, 1 is Monday, etc.)
 
-            console.log(`Event date: ${eventDateISO}`);
-            const matchingDay = Array.from(days).find(day => day.getAttribute('data-date') === eventDateISO);
-            if (matchingDay) {
-                console.log(`Matching day attribute: ${matchingDay.getAttribute('data-date')}`);
-                const timeSlot = matchingDay.querySelector(`.time-slot[data-hour="${startHour}"]`);
-                console.log(`Looking for time slot at hour: ${startHour}, found:`, timeSlot);
-                if (timeSlot) {
-                    timeSlot.appendChild(eventElement);
-                } else {
-                    console.error('No time slot found for event:', event);
-                }
-            }
-        });
-    }
+        // Find the matching day and time slot
+        const dayElement = days[eventDay]; // This assumes that the days NodeList is in the correct order (Sunday to Saturday)
+        const timeSlot = dayElement.querySelector(`.time-slot[data-hour="${startHour}"]`);
 
-    updateCalendar(currentWeekStart);
-    fetchEvents();
+        if (timeSlot) {
+            timeSlot.appendChild(eventElement);
+        } else {
+            console.error('No time slot found for event:', event);
+        }
+    });
+}
+
 
     document.getElementById('prevWeek').addEventListener('click', function() {
         currentWeekStart.setDate(currentWeekStart.getDate() - 7);
         updateCalendar(currentWeekStart);
-        fetchEvents();
+        fetchEvents(); 
     });
 
     document.getElementById('nextWeek').addEventListener('click', function() {
         currentWeekStart.setDate(currentWeekStart.getDate() + 7);
         updateCalendar(currentWeekStart);
-        fetchEvents();
+        fetchEvents(); 
     });
+
+    updateCalendar(currentWeekStart); 
+    fetchEvents(); 
 };
