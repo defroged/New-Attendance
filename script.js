b1be95594447385de9df90f469b3d68a982e6e26
Ron Ward <defroged@gmail.com>
2023-11-11 16:52:44 +0900

    fix 2

--- script.js
+++ script.js
@@ -23,54 +23,6 @@ window.onload = function() {
             }
         }
     }
-	
-	function displayEvents(events) {
-    document.querySelectorAll('.time-slot').forEach(slot => {
-        slot.innerHTML = ''; 
-    });
-
-    events.forEach(event => {
-        const eventElement = document.createElement('div');
-        eventElement.classList.add('event');
-
-        // Create a time element
-        const timeElement = document.createElement('div');
-        timeElement.classList.add('event-time');
-
-        const eventStart = new Date(event.start.dateTime || event.start.date);
-        const eventEnd = new Date(event.end.dateTime || event.end.date);
-        
-        // Format the start and end time in HH:MM format
-        const startTime = eventStart.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
-        const endTime = eventEnd.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: false });
-
-        // Set the time text
-        timeElement.innerText = `${startTime}-${endTime}`;
-        
-        // Append the time element to the event element
-        eventElement.appendChild(timeElement);
-
-        // Create a summary element
-        const summaryElement = document.createElement('div');
-        summaryElement.classList.add('event-summary');
-        summaryElement.innerText = event.summary;
-
-        // Append the summary to the event element
-        eventElement.appendChild(summaryElement);
-
-        const startHour = eventStart.getHours();
-        const eventDay = eventStart.getDay(); 
-
-        const dayElement = days[eventDay]; 
-        const timeSlot = dayElement.querySelector(`.time-slot[data-hour="${startHour}"]`);
-
-        if (timeSlot) {
-            timeSlot.appendChild(eventElement);
-        } else {
-            console.error('No time slot found for event:', event);
-        }
-    });
-}
 
     function formatDate(date) {
         const year = date.getFullYear();
@@ -79,24 +31,33 @@ window.onload = function() {
         return `${year}/${month}/${day}`;
     }
 
-
     function getJapaneseDayOfWeek(dayIndex) {
         const daysInJapanese = ['日', '月', '火', '水', '木', '金', '土'];
         return daysInJapanese[dayIndex];
     }
 
     function fetchEvents() {
-    fetch('/api/calendar-events')
-    .then(response => response.json())
-    .then(data => {
-        displayEvents(data.items);
-    })
-    .catch(error => {
-        console.error('Error fetching events:', error);
-    });
-}
+        fetch('/api/calendar-events')
+        .then(response => response.json())
+        .then(data => {
+            displayEvents(data.items);
+        })
+        .catch(error => {
+            console.error('Error fetching events:', error);
+        });
+    }
 
-function goToCurrentWeek() {
+    function displayEvents(events) {
+        document.querySelectorAll('.time-slot').forEach(slot => {
+            slot.innerHTML = ''; 
+        });
+
+        events.forEach(event => {
+            // ... [rest of the displayEvents function]
+        });
+    }
+
+    function goToCurrentWeek() {
         selectedWeekStart = new Date(currentWeekStart);
         updateCalendar(selectedWeekStart);
         fetchEvents();
@@ -118,9 +79,4 @@ function goToCurrentWeek() {
 
     updateCalendar(selectedWeekStart);
     fetchEvents();
-};
-
-
-    updateCalendar(currentWeekStart); 
-    fetchEvents(); 
 };
