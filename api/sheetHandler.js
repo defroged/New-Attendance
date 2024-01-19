763863be6d595300ff02b32b8d749921eeb19e98
Ron Ward <defroged@gmail.com>
2024-01-20 07:23:40 +0900

    fix2

--- api/sheetHandler.js
+++ api/sheetHandler.js
@@ -2,14 +2,14 @@ const fetch = require('node-fetch');
 const {google} = require('googleapis');
 
 // Initialize Google Sheets API client and authorize
-const sheets = google.sheets({version: 'v4', auth: new google.auth.JWT(
-    process.env.GOOGLE_CLIENT_EMAIL,
-    null,
-    process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    ['https://www.googleapis.com/auth/spreadsheets']
-  )
-});
-sheets._options.auth = authClient;
+const authClient = new google.auth.JWT(
+  process.env.GOOGLE_CLIENT_EMAIL,
+  null,
+  process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  ['https://www.googleapis.com/auth/spreadsheets']
+);
+
+const sheets = google.sheets({version: 'v4', auth: authClient});
 
 async function updateAttendance(spreadsheetId, range, data) {
   try {
