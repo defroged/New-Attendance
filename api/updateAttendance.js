d49038307d645d0732897fbb124c3a57eb65a39c
Ron Ward <defroged@gmail.com>
2024-02-09 15:24:05 +0900

    added log

--- api/updateAttendance.js
+++ api/updateAttendance.js
@@ -2,11 +2,11 @@ const sheetHandler = require('./sheetHandler');
 
 module.exports = async (req, res) => {
   try {
-    const { spreadsheetId, range, data } = req.body; // Extract properties from req.body
-    const result = await sheetHandler.updateAttendance(spreadsheetId, range, data); // Pass arguments correctly
+    const { spreadsheetId, range, data } = req.body; 
+    const result = await sheetHandler.updateAttendance(spreadsheetId, range, data); 
     res.status(200).json(result);
   } catch (error) {
     console.error('Error in updateAttendance route:', error);
-    res.status(500).send('Internal Server Error');
-  }
+    res.status(500).json({ message: error.message });
+}
 };
