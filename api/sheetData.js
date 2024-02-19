ee47b4a1842d5e559f45020638216114fb3c664c
Ron Ward <defroged@gmail.com>
2024-02-19 20:16:37 +0900

    fix 1

--- api/sheetData.js
+++ api/sheetData.js
@@ -1,11 +1,11 @@
-const sheetHandler = require('./sheetHandler');
+const { fetchSheetData } = require('./sheetHandler');
 
 module.exports = async (req, res) => {
-  try {
-    const result = await sheetHandler(req, res);
-    res.status(200).json(result);
-  } catch (error) {
-    console.error('Error in classDetails route:', error);
-    res.status(500).send('Internal Server Error');
-  }
+try {
+  const result = await fetchSheetData(req, res);
+  res.status(200).json(result);
+} catch (error) {
+  console.error('Error in classDetails route:', error);
+  res.status(500).send('Internal Server Error');
+}
 };
--- api/sheetHandler.js
+++ api/sheetHandler.js
@@ -45,29 +45,7 @@ const stringData = data.map(row => {
   }
 }
 
-/*module.exports = async (req, res) => {
-  const spreadsheetId = '1ax9LCCUn1sT6ogfZ4sv9Qj9Nx6tdAB-lQ3JYxdHIF7U';
-  const apiKey = process.env.GOOGLE_CALENDAR_API_KEY;
-  const sheetName = 'Sheet1';
-  const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${apiKey}`;
 
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-  try {
-    const sheetResponse = await fetch(url);
-    if (!sheetResponse.ok) {
-      throw new Error(`Failed to fetch Google Sheet data: ${sheetResponse.statusText}`);
-    }
-    const sheetData = await sheetResponse.json();
-    res.status(200).json(sheetData);
-  } catch (error) {
-    console.error('Error fetching Google Sheet data:', error);
-    res.status(500).send('Internal Server Error');
-  }
-};*/
 
 async function findStudentRowIndex(student, spreadsheetId, sheetName) {
   const response = await sheets.spreadsheets.values.get({
@@ -135,6 +113,29 @@ async function updateAbsenceDates(spreadsheetId, sheetName, absenceData) {
 }
 
 module.exports = {
+  fetchSheetData: async (req, res) => {
+    const spreadsheetId = '1ax9LCCUn1sT6ogfZ4sv9Qj9Nx6tdAB-lQ3JYxdHIF7U';
+    const apiKey = process.env.GOOGLE_CALENDAR_API_KEY;
+    const sheetName = 'Sheet1';
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${sheetName}?key=${apiKey}`;
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    try {
+      const sheetResponse = await fetch(url);
+      if (!sheetResponse.ok) {
+        throw new Error(`Failed to fetch Google Sheet data: ${sheetResponse.statusText}`);
+      }
+      const sheetData = await sheetResponse.json();
+      res.status(200).json(sheetData);
+    } catch (error) {
+      console.error('Error fetching Google Sheet data:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  },
   updateAttendance,
   updateAbsenceDates,
 };
