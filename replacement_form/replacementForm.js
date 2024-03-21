0d9ca35b041b4a7e47aea3c299f06e46e0a87c16
Ron Ward <defroged@gmail.com>
2024-03-21 20:09:04 +0900

    adding more

--- replacement_form/replacementForm.js
+++ replacement_form/replacementForm.js
@@ -120,6 +120,7 @@ async function handleLogin() {
 }
 
 async function handleStudentNameMatch(studentName) {
+		showSpinner();
   document.getElementById("main-container").style.display = "block";
   document.getElementById("login-section").style.display = "none";
   document.getElementById("step-one").style.display = "none";
@@ -144,6 +145,7 @@ async function handleStudentNameMatch(studentName) {
   
   const availableSlots = parseInt(document.getElementById("available-slots").getAttribute("data-count"), 10);
   displayAvailableSlots(availableSlots);
+  hideSpinner(); 
 }
 
 function displaySubmitSectionIfRequired() {
