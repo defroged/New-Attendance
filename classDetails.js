5c0d8a576fa1af1c4796c5d348921c45df926d79
Ron Ward <defroged@gmail.com>
2024-04-13 14:55:59 +0900

    better text 2

--- classDetails.js
+++ classDetails.js
@@ -64,7 +64,7 @@ function findReplacementStudents(data, date) {
 
 function showModalWithClassDetails(className, students, eventDate, replacementStudents, eventDetails) {
     const formattedEventDate = eventDate.replace(/-/g, "/");
-	const formattedDescription = eventDetails.description.replace(/\n/g, '<br>'); 
+	const formattedDescription = eventDetails.description.replace(/\n/g, '<br><br>'); 
     let modalContent = `<h4>Class: ${className}</h4>
                         <p>Description: ${eventDetails.description}</p>
                         <p><a href="${eventDetails.location}" target="_blank">Lesson Report</a></p>
