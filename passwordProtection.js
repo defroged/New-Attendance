f71ae5c4c7cec9b2b4cca138ccfab9c09dc64f47
Ron Ward <defroged@gmail.com>
2024-05-11 09:45:50 +0900

    Rollback

--- passwordProtection.js
+++ passwordProtection.js
@@ -1,28 +1,18 @@
 async function checkPasswordAndRevealContent() {
-  const passwordModal = document.getElementById('passwordModal');
-  const passwordInput = document.getElementById('passwordInput');
-  const submitButton = document.getElementById('submitButton');
+  let password = prompt("Please enter the password to access the page:");
+  const response = await fetch('/api/checkPagePassword', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ password })
+  });
 
-  passwordModal.style.display = 'block';
-
-  submitButton.onclick = async () => {
-    let password = passwordInput.value;
-
-    const response = await fetch('/api/checkPagePassword', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ password }),
-    });
-
-    if (response.status === 200) {
-      document.getElementById('pageBody').style.display = 'block';
-      scrollToCurrentTime();
-      passwordModal.style.display = 'none';
-    } else {
-      alert('Incorrect password.');
-      window.location.href = 'index.html';
-    }
-  };
+  if (response.status === 200) {
+    document.getElementById('pageBody').style.display = 'block';
+    scrollToCurrentTime();
+  } else {
+    alert('Incorrect password.');
+    window.location.href = 'index.html';
+  }
 }
